7795376ea193c67aa943c34ebbbd7ce681d16452
lukluk <luklukaha@gmail.com>
2016-03-04 07:21:29 -0500

    test

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.get('/cekresi/:resi', function(req, res) {
 	horseman
 		.userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0")
 		.open('http://cekresi.com/?noresi='+req.params.resi)
+		.click("#cekresi")
 		.waitForSelector('#results table')
 		.evaluate(function() {
 			return document.querySelector('#results').innerHTML;
